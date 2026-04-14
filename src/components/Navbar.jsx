0ba118701e72db632b2daa153ea15796cea41162
bharathguntreddi3 <guntreddibharath@gmail.com>
2026-04-14 18:16:52 +0530

    git nav to mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -620,8 +620,8 @@ export default function Navbar() {
             border-bottom: 1px solid rgba(255,255,255,0.1);
             box-shadow: 0 4px 30px rgba(0, 0, 0, 0.15);
             
-            // env(safe-area-inset-top) safely ignores the notification pane and nav pane in mobile screens
-            padding: max(10px, env(safe-area-inset-top)) 0 10px 0; 
+            /* safely ignores the notification pane and nav pane in mobile screens */
+            padding: max(15px, env(safe-area-inset-top)) 0 10px 0; 
             
             transition: all 0.3s ease;
           }
